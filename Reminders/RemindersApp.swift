7e476a1eff1cc045563e59c959ee137d76e01517
Victor Deon <victorhad@gmail.com>
2025-05-22 10:16:56 -0300

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Victor Deon on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
